032029555af104523e7350b7abe4072b1117df42
arturo.olivares <docaoc1@gmail.com>
2024-07-08 15:59:12 -0600

    1.012

--- Medico/signals.py
+++ Medico/signals.py
@@ -3,28 +3,33 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Resumen, Doctor, Asignacion, Especialidad
 from django.core.mail import send_mail
+from django.db import transaction
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Resumen, Doctor, Asignacion, Especialidad
+from django.core.mail import send_mail
 
-def obtener_siguiente_becario(especialidad):
-    becarios = list(Doctor.objects.filter(especialidad=especialidad, tipo='Becario').order_by('id'))
+def obtener_siguiente_medico(especialidad, tipo_medico):
+    medicos = list(Doctor.objects.filter(especialidad=especialidad, tipo=tipo_medico).order_by('id'))
     
-    if not becarios:
+    if not medicos:
         return None
 
     asignacion, created = Asignacion.objects.get_or_create(
         especialidad=especialidad,
-        tipo_medico='Becario',
-        defaults={'ultimo_medico': becarios[0]}  # Usar el primer becario en la lista
+        tipo_medico=tipo_medico,
+        defaults={'ultimo_medico': medicos[0]}  # Usar el primer médico en la lista
     )
 
     try:
-        siguiente_becario = becarios[becarios.index(asignacion.ultimo_medico) + 1]
+        siguiente_medico = medicos[medicos.index(asignacion.ultimo_medico) + 1]
     except (ValueError, IndexError):
-        siguiente_becario = becarios[0]
+        siguiente_medico = medicos[0]
 
-    asignacion.ultimo_medico = siguiente_becario
+    asignacion.ultimo_medico = siguiente_medico
     asignacion.save()
 
-    return siguiente_becario
+    return siguiente_medico
 
 @receiver(post_save, sender=Resumen)
 def asignar_medicos(sender, instance, created, **kwargs):
@@ -32,30 +37,30 @@ def asignar_medicos(sender, instance, created, **kwargs):
         especialidad = instance.especialidad
 
         with transaction.atomic():
-            # Asignar todos los adscritos de la especialidad
-            adscritos = Doctor.objects.filter(especialidad=especialidad, tipo='Adscrito')
-            adscritos_emails = []
-
-            for adscrito in adscritos:
+            # Asignar un adscrito de la especialidad
+            adscrito = obtener_siguiente_medico(especialidad, 'Adscrito')
+            if adscrito:
                 instance.medico_adscrito.add(adscrito)
-                adscritos_emails.append(adscrito.email)
-            
+                adscrito_email = [adscrito.email]
+            else:
+                adscrito_email = []
+
             # Asignar un becario de la especialidad
-            becario = obtener_siguiente_becario(especialidad)
+            becario = obtener_siguiente_medico(especialidad, 'Becario')
             instance.medico_becario = becario
 
             # Guardar el resumen con los médicos asignados
             instance.save()
 
             #Enviar correos de notificacion
-            if adscritos_emails or becario:
+            recipient_list = adscrito_email
+            if becario:
+                recipient_list.append(becario.email)
+
+            if recipient_list:
                 subject = 'Nuevo resumen medico asignado'
                 message = f'Se ha asignado un nuevo resumen con el numero de expediente {instance.numero_expediente}. Revisalo en tu plataforma de resumenes medicos en la siguiente dirección: resumenesimo.ddns.net'
                 from_email = 'arturo.olivares@imoiap.com.mx'
-
-                recipient_list = adscritos_emails
-                if becario:
-                    recipient_list.append(becario.email)
 
                 try:
                     send_mail(subject, message, from_email, recipient_list)
