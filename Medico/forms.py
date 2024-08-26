23e29eb31ccb0afdf9f7ab3b96a2ade3a876d33a
arturo.olivares <docaoc1@gmail.com>
2024-08-26 12:09:14 -0600

    Cambio a summernote

--- Medico/forms.py
+++ Medico/forms.py
@@ -1,11 +1,10 @@
 from django import forms
 from .models import Documento,Resumen
-from froala_editor.widgets import FroalaEditor
 
 class ResumenForm(forms.ModelForm):
     class Meta:
         model = Resumen
         fields = ['texto']
         widgets = {
-            'texto': FroalaEditor(),
+            'texto': forms.Textarea()
         }
