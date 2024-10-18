537e76fc8e46cf7852efd4c6fad2685501ab67a0
arturo.olivares <docaoc1@gmail.com>
2024-10-18 12:29:07 -0600

    Eliminacion a referencia .env desde dotenv

--- Resumen/settings.py
+++ Resumen/settings.py
@@ -14,9 +14,9 @@ from pathlib import Path
 import os
 from django.contrib.messages import constants as messages
 from cryptography.fernet import Fernet
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
-load_dotenv()
+#load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
